e22009ee135e9f5ca3d87fbd7ee9424403c7e2f8
boksii17 <bn20190210@student.fon.bg.ac.rs>
2023-07-10 22:39:34 +0200

    api rute

--- example-app/routes/api.php
+++ example-app/routes/api.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Http\Controllers\AmbulantaController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\OpstinaController;
+use App\Http\Controllers\PorukaController;
+use App\Http\Controllers\SkolaController;
+use App\Http\Controllers\VakcinaController;
+use App\Http\Controllers\ZakazivanjeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +20,58 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+Route::get('vakcine/{id}', [VakcinaController::class, 'show']);
+Route::get('vakcine', [VakcinaController::class, 'index']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('opstine/{id}', [OpstinaController::class, 'show']);
+Route::get('opstine', [OpstinaController::class, 'index']);
+
+Route::get('ambulante/{id}', [AmbulantaController::class, 'show']);
+Route::get('ambulante', [AmbulantaController::class, 'index']);
+
+Route::put('ambulante/{id}', [AmbulantaController::class, 'update']);
+Route::delete('ambulante/{id}', [AmbulantaController::class, 'destroy']); 
+Route::post('ambulante', [AmbulantaController::class, 'store']);
+
+ 
+Route::get('skole/{id}', [SkolaController::class, 'show']);
+Route::get('skole', [SkolaController::class, 'index']);
+
+Route::put('skole/{id}', [SkolaController::class, 'update']);
+Route::delete('skole/{id}', [SkolaController::class, 'destroy']); 
+Route::post('skole', [SkolaController::class, 'store']);
+
+
+
+
+
+
+Route::get('zakazivanja/{id}', [ZakazivanjeController::class, 'show']);
+Route::get('zakazivanja', [ZakazivanjeController::class, 'index']);
+Route::post('zakazivanja', [ZakazivanjeController::class, 'store']);
+Route::get('poruke',[PorukaController::class,'index']);  
+Route::post('kontakt', [PorukaController::class, 'primiPoruku']); 
+Route::group(['middleware' => ['auth:sanctum']], function () {   
+    Route::get('/profiles', function (Request $request) {  
+        return auth()->user();
+    });
+
+  
+ 
+    
+   
+   
+    Route::post('logout', [AuthController::class, 'logout']);  
+  
+});
+Route::middleware(['auth:sanctum','isAPIAdmin'])->group(function(){  
+
+    Route::get('/proveri', function(){
+        return response()->json(['message'=>'Admin je ulogovan','status'=>200],200);
+    });
+    Route::post('logout', [AuthController::class, 'logout']);  
+   
+
 });
